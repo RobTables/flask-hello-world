bfaf45fb6479fa6caa06abbadc67ae78ba2e3c16
RobTables <52382103+RobTables@users.noreply.github.com>
2025-07-30 04:19:34 +0000

    db

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import psycopg
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Andrew in 3308'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg.connect('postgresql://flasklab_user:BkBajs8ePSBumCOTvAlLqOTRd9gQhbYe@dpg-d24pmt7gi27c73ba47rg-a/flasklab')
+    conn.close()
+    return 'Database Connection Successful'
